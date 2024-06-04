a24467472ddd6db19500e384fe9904860332731c
Brandon Fyall <BrandonFyall@Brandons-MacBook-Pro-2.local>
2024-06-04 08:58:49 -0700

    initial commit

---
+++ src/state/usePageState.ts
@@ -0,0 +1,73 @@
+import { Page, NodeData, NodeType } from "../utils/types"
+import { arrayMove } from "@dnd-kit/sortable";
+import { useSyncedState } from "./useSyncedState";
+import { updatePage } from "../utils/updatePage";
+
+export const usePageState = (initialState: Page) => {
+    const [page, setPage] = useSyncedState(initialState, updatePage);
+
+    const addNode = (node: NodeData, index: number) => {
+        setPage((draft) => {
+            const newNodes = [...draft.nodes]; // Create a copy of the array
+            newNodes.splice(index, 0, node); // Modify the copy
+            draft.nodes = newNodes; // Set the modified copy back to the draft
+        });
+    }
+
+    const removeNodeByIndex = (nodeIndex: number) => {
+        setPage(draft => draft.nodes.splice(nodeIndex, 1))
+    }
+
+    const changeNodeValue = (nodeIndex: number, value: string) => {
+        setPage((draft) => {
+            draft.nodes[nodeIndex].value = value
+        });
+    };
+
+    const changeNodeType = (nodeIndex: number, type: NodeType) => {
+        setPage(draft => {
+            draft.nodes[nodeIndex].type = type;
+            draft.nodes[nodeIndex].value = ""
+        })
+    }
+
+    const setNodes = (nodes: NodeData[]) => {
+        setPage((draft) => {
+            draft.nodes = nodes;
+        })
+    }
+
+    const setTitle = (title: string) => {
+        setPage((draft) => {
+            draft.title = title
+        });
+    }
+
+    const setCoverImage = (coverImage: string) => {
+        setPage((draft) => {
+            draft.cover = coverImage;
+        })
+    }
+
+    const reorderNodes = (id1: string, id2: string) => {
+        setPage((draft) => {
+            const index1 = draft.nodes.findIndex(node => node.id === id1)
+            const index2 = draft.nodes.findIndex(node => node.id === id2)
+            draft.nodes = arrayMove(draft.nodes, index1, index2)
+        })
+    }
+
+    return {
+        nodes: page.nodes,
+        title: page.title,
+        cover: page.cover,
+        changeNodeType,
+        changeNodeValue,
+        addNode,
+        removeNodeByIndex,
+        setTitle,
+        setCoverImage,
+        setNodes,
+        reorderNodes
+    }
+}
